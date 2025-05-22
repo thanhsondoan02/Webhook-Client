a9ead75bde5876e5f068f219f93e5b62c2859101
thanhsondoan02 <thanhsondoan02@gmail.com>
2025-05-22 15:03:37 +0700

    init project

---
+++ src/main/java/com/peswoc/hookclient/HookClientApplication.java
@@ -0,0 +1,13 @@
+package com.peswoc.hookclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HookClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peswoc/hookclient/HookClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peswoc.hookclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
